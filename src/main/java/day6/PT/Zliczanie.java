c2f92f9bc6bcedda28cdb14ad4f5c8eab005ee00
Pawel Ujazdowski <uio6398@gmail.com>
2019-08-11 22:14:22 +0200

    day6

--- src/main/java/day6/PT/Zliczanie.java
+++ src/main/java/day6/PT/Zliczanie.java
@@ -3,14 +3,51 @@ package day6.PT;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Zliczanie {
 
-    private int podajMax(int minIloscLiter) throws IOException {
+    private static String podajMax(int minIloscLiter) throws IOException {
+        Map<String, Integer> mapaSlow = zlicz(minIloscLiter, getLines());
+        return podajMax(mapaSlow);
+    }
 
-        List<String> wiersze = Files.readAllLines(Paths.get("pan-tadeusz.txt"));
+    private static String podajMax(Map<String, Integer> mapaSlow) {
+        int i = 0;
+        String s = "";
+        for (Map.Entry<String, Integer> m : mapaSlow.entrySet()) {
+            if (m.getValue() > i) {
+                i = m.getValue();
+                s = m.getKey();
+            }
+        }
+        return s + " wystepuje najczesciej w PT " + i;
+    }
 
-        
+    private static Map<String, Integer> zlicz(int minIloscLiter, List<String> wiersze) {
+        Map<String, Integer> mapaSlow = new HashMap<>();
+        for (String wiersz : wiersze) {
+            String[] slowa = wiersz.split("[\\s,!?:.\\-]+");
+            for (String slowo : slowa) {
+                if (slowo.length() >= minIloscLiter) {
+                    if (mapaSlow.containsKey(slowo)) {
+                        mapaSlow.put(slowo, mapaSlow.get(slowo) + 1);
+                    } else {
+                        mapaSlow.put(slowo, 1);
+                    }
+                }
+            }
+        }
+        return mapaSlow;
+    }
+
+    private static List<String> getLines() throws IOException {
+        return Files.readAllLines(Paths.get("pan-tadeusz.txt"));
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(podajMax(7));
     }
 }
---
+++ src/main/java/day6/Palindrom.java
@@ -0,0 +1,38 @@
+package day6;
+
+public class Palindrom {
+    public static void main(String[] args) {
+        System.out.println(czyPalindrom2("Eva, can I see bees in a cave?"));
+    }
+
+    private static boolean czyPalindrom (String slowo) {
+//        String temp = slowo.replace(",", "")
+//                .replace(" ", "")
+//                .replace("?","").toLowerCase();
+
+        String temp = slowo.replaceAll("[\\s,.?!]+", "").toLowerCase();
+        String odwrocony = new StringBuilder(temp).reverse().toString();
+        return temp.equals(odwrocony);
+    }
+
+    private static boolean czyPalindrom1(String slowo) {
+        String isPalindrom="";
+        String temp = slowo.replaceAll("[\\s,.?!]+", "").toLowerCase();
+        for (int i = temp.length() - 1; i >= 0; i--){
+            isPalindrom += temp.charAt(i);
+        }
+        return temp.equals(isPalindrom);
+    }
+
+    private static boolean czyPalindrom2 (String slowo){
+
+        String temp = slowo.replaceAll("[\\s,.?!]+", "").toLowerCase();
+        char[] tempchar = temp.toCharArray();
+        for (int i = 0; i < temp.length()/2; i++){
+            if (tempchar[i] != tempchar[tempchar.length-i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
